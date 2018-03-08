cc5aa02816472d78588100a9c263ccb899102d11
= <=>
2018-03-08 15:59:10 +0600

    added class 42

---
+++ resources/views/notes/index.blade.php
@@ -0,0 +1,21 @@
+@extends('master')
+@section('content')
+@foreach($notes as $note)
+<div class="card my-2">
+  <div class="card-body">
+    <h4>
+      <a href="{{route('notes.show', $note->id)}}">
+        {{$note->title}}
+      </a>
+      <a href="{{route('notes.edit', $note->id)}}" class="btn btn-outline-info">Edit</a>
+      <form class="d-inline" action="{{route('notes.destroy', $note->id)}}" method="post">
+        @csrf
+        @method('delete')
+        <button type="submit" class="btn btn-outline-danger">Delete</button>
+      </form>
+    </h4>
+  </div>
+</div>
+@endforeach
+{{$notes->links()}}
+@endsection
